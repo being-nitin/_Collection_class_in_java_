60ca77cce78d611231ee5b331cc7201da441be4f
Nitin <nsinghboss02@gmail.com>
2022-03-13 14:32:11 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,29 @@
+package com.Recursion;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class Main {
+
+    public static void main(String[] args) {
+	// Collection class:
+        List <Integer> list = new ArrayList<>();
+        list.add(23);
+        list.add(21);
+        list.add(26);
+        list.add(56);
+        list.add(15);
+
+        // for min element
+        System.out.println(Collections.min(list));
+    
+
+
+
+
+    }
+
+
+
+
+
+    }
